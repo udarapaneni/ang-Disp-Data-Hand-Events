d59bda58f6b25d0585993db1e63693d7f1c041a4
Umakanth Darapaneni <umakanth158@gmail.com>
2021-02-21 01:01:12 +0530

    event-binding-bubbling

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  template: ` <button (click)="onSave($event)">Save</button> `,
+  template: `
+    <div (click)="onDivClick()">
+      <button (click)="onSave($event)">Save</button>
+    </div>
+  `,
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-  onSave($event: {}): void {
+  onSave($event: any): void {
+    $event.stopPropagation(); // to stop event bubbling
     console.log('Saving', $event);
+  }
+  onDivClick(): void {
+    console.log('Div Clicked');
   }
 }
