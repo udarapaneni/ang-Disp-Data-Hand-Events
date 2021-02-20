bf52b851b96916ddc36802fc3839db22d158387b
Umakanth Darapaneni <umakanth158@gmail.com>
2021-02-21 00:29:52 +0530

    style-binding

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  template: `
+    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
+  `,
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  isActive = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
