83b63c9a1de97eb4ab14fe71f298d6132e31170f
Umakanth Darapaneni <umakanth158@gmail.com>
2021-02-21 12:17:54 +0530

    event-filtering

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,19 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  template: `
-    <div (click)="onDivClick()">
-      <button (click)="onSave($event)">Save</button>
-    </div>
-  `,
+  template: ` <input (keyup)="onKeyUp($event)" /> `,
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-  onSave($event: any): void {
-    $event.stopPropagation(); // to stop event bubbling
-    console.log('Saving', $event);
-  }
-  onDivClick(): void {
-    console.log('Div Clicked');
+  onKeyUp($event: any): void {
+    if ($event.keyCode === 13) {
+      console.log('Enter was entered');
+    }
+    // console.log('Saving', $event);
   }
 }
