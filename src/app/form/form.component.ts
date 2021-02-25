3365437144f5cb38165cdba65eceba33cec0a815
Umakanth Darapaneni <umakanth158@gmail.com>
2021-02-25 13:22:23 +0530

    noim

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  template: ` <input (keyup)="onKeyUp($event)" /> `,
+  template: ` <input #email (keyup.enter)="onKeyUp(email.value)" /> `,
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-  onKeyUp($event: any): void {
-    if ($event.keyCode === 13) {
-      console.log('Enter was entered');
-    }
-    // console.log('Saving', $event);
+  onKeyUp(email: any): void {
+    console.log(email);
+    //  console.log('Saving', $event);
   }
 }
